dc51fe3ba8b3fa22fa328948f6da7e6648a0c1b6
Thomas Heinemann <data1701-e@web.de>
2017-07-09 23:29:51 +0200

    Move registerSwitch to switchControl class

--- switchControl/src/python/switchControl/switchControl.py
+++ switchControl/src/python/switchControl/switchControl.py
@@ -1,11 +1,30 @@
 
 from enum import Enum
+import sys
+try:
+    import RPi.GPIO as GPIO
+except ModuleNotFoundError:
+    sys.stdout.write("Could not import RPi.GPIO - Will continue with GPIO Mock.\n")
+    sys.stdout.write("If you are running this program on a Raspberry Pi, this is probably not what you want.\n")
+    sys.stdout.write("The GPIO pins will not actually be changed, i.e. connected devices are not controlled.\n")
+    import gpioMock as GPIO
 
+class Direction(Enum):
+    straight = 1
+    turn = 2
 
 class SwitchControl:
-    _switches = {}
-    _nextSwitch = 0
-    _directions = {
-        'straight': GPIO.HIGH,
-        'turn': GPIO.LOW
-    }
+    def __init__(self):
+        self.switches = {}
+        self.nextSwitch = 0
+
+    def registerSwitch(self, pin):
+        if pin in self.switches.values():
+            switch = list(self.switches.keys())[list(self.switches.values()).index(pin)]
+            raise RuntimeError("Pin {} already is registered as switch {}".format(pin, switch))
+        switchId = self.nextSwitch
+        self.switches[switchId] = pin
+        GPIO.setup(pin, GPIO.OUT)
+        GPIO.output(pin, GPIO.HIGH)
+        self.nextSwitch = switchId + 1
+        return switchId
--- switchControl/src/python/switchControl/switchControl_test.py
+++ switchControl/src/python/switchControl/switchControl_test.py
@@ -1,6 +1,23 @@
 
 import unittest
+from switchControl.switchControl import SwitchControl
 
 class SwitchControlTest(unittest.TestCase):
-    def test_turn(self):
-        self.assertTrue(False, msg="Not implemented yet.")
+    def test_registerSwitch(self):
+        sc = SwitchControl()
+        sc.registerSwitch(3)
+        self.assertEqual(1, len(sc.switches))
+        self.assertTrue(0 in sc.switches)
+        self.assertEqual(3, sc.switches[0])
+        self.assertEqual(1, sc.nextSwitch)
+
+        with self.assertRaises(RuntimeError):
+            sc.registerSwitch(3)
+
+        sc.registerSwitch(4)
+        self.assertEqual(2, len(sc.switches))
+        self.assertTrue(0 in sc.switches)
+        self.assertTrue(1 in sc.switches)
+        self.assertEqual(3, sc.switches[0])
+        self.assertEqual(4, sc.switches[1])
+        self.assertEqual(2, sc.nextSwitch)
