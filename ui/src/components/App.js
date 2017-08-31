f169200029ec6c9e614ec8c3bc8d8cdc6157c182
Thomas Heinemann <data1701-e@web.de>
2017-08-31 23:16:04 +0200

    Add infrastructure for UI

---
+++ ui/src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = React.createClass({
+  render() {
+    return (
+     <div style={{textAlign: 'center'}}>
+        <h1>Hello World</h1>
+        This is the test screen. Content will follow soon!
+      </div>);
+  }
+})
+
+export default App;
