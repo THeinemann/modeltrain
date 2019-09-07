392ba0c9bcef796c87cbc618119d755406df2e75
Thomas Heinemann <data1701-e@web.de>
2019-09-07 23:41:14 +0200

    Implement setup and loop methods for Arduino

--- speedControl/src/main.cpp
+++ speedControl/src/main.cpp
@@ -1,12 +1,19 @@
-#include <stdio.h>
+#include "interface.h"
+
+auto directionService = DirectionService(6, 7);
+auto speedService = SpeedService(9);
+
+auto controller = Controller(directionService, speedService);
+auto interface = Interface(controller);
 
 extern "C" {
 void setup() {
-
+  directionService.setDirection(protocol::FORWARD);
+  speedService.setSpeed(0);
 }
 
 void loop() {
-
+  interface.process();
 }
 
 }
