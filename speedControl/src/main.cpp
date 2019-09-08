d4f6f055ca0486717b620af87eb9a88d03f773f3
Thomas Heinemann <data1701-e@web.de>
2019-09-08 22:46:26 +0200

    Start Serial communication on Arduino startup

--- speedControl/src/main.cpp
+++ speedControl/src/main.cpp
@@ -1,4 +1,5 @@
 #include "interface.h"
+#include "arduino_wrapper.hpp"
 
 auto directionService = DirectionService(6, 7);
 auto speedService = SpeedService(9);
@@ -8,6 +9,7 @@ auto interface = Interface(controller);
 
 extern "C" {
 void setup() {
+  Serial.begin(9600);
   directionService.setDirection(protocol::FORWARD);
   speedService.setSpeed(0);
 }
